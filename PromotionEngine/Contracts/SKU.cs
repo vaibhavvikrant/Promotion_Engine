97a41898a036eb12c9270cd7a1590d0380d4b512
Vaibhav <682139@corp.trizetto.com>
2020-09-17 11:07:14 +0530

    Add project files.

---
+++ PromotionEngine/Contracts/SKU.cs
@@ -0,0 +1,34 @@
+﻿using PromotionEngine.Interface;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Contracts
+{
+    public class SKU
+    {
+        private readonly List<IPromotion> _appliedPromotion = new List<IPromotion>();
+        public string Id { get; set; }
+        public decimal OriginalPrice { get; set; }
+
+        public decimal PromotionalPrice
+        {
+            get 
+            {
+                decimal promotionalPrice = OriginalPrice;
+                return promotionalPrice;
+            }
+        }
+        public SKU(string id, decimal price)
+        {
+            Id = id;
+            OriginalPrice = price;
+        }
+
+        public List<IPromotion> ApplyPromotion
+        {
+            get
+            {
+                return _appliedPromotion;
+            }
+        }
+    }
+}
---
+++ PromotionEngine/Interface/IPromotion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Interface
+{
+    public interface IPromotion
+    {
+        decimal ApplyPromotion(decimal skuPrice);
+    }
+}
