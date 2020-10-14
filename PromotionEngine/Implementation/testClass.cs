cb5cc37c93b1293830a15d10c32b3859c8d97ffa
Vikrant Vaibhav <vaibhavvikrant@gmail.com>
2020-10-14 14:55:36 +0530

    Addign test method

---
+++ PromotionEngine/Implementation/testClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PromotionEngine.Implementation
+{
+    class testClass
+    {
+        public static List<string> processData(IEnumerable<string> lines)
+        {
+            static int ConvertToInt(string obj)
+            {
+                int.TryParse(obj, out var intVal);
+                return intVal;
+            }
+
+            var splittedLines = lines.Select(obj => obj.Split(','));
+            var discountedRows = splittedLines.GroupBy(obj => obj[3]).SelectMany(obj =>
+            obj.Where(obj1 =>
+                            obj1[3] == obj.Key &&
+                            ConvertToInt(obj1[4]) < obj.Max(obj => ConvertToInt(obj[4]))).
+                  Select(a => a)).Distinct().ToList();
+            var finalRows = discountedRows.GroupBy(obj => obj[3]).SelectMany(
+                obj => obj.Where(obj1 => obj1[3] == obj.Key && ConvertToInt(obj1[4]) == obj.Min(obj => ConvertToInt(obj[4]))).
+                  Select(a => a[0])).Distinct().ToList();
+            return finalRows;
+        }
+    }
+}
